39acccca973044933ef26970aaa317303c89f6e2
alexandra <alexandra.dolca@student.upt.ro>
2025-02-14 12:00:19 +0300

    First commit with project structure

---
+++ src/test/java/com/modul2/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modul2.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
