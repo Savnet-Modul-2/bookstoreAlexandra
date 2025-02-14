02ce68483a5302ece77aad8eb4cecc7e6e234c85
alexandra <alexandra.dolca@student.upt.ro>
2025-02-14 15:11:19 +0300

    MD5 encoded password

--- src/main/java/com/modul2/bookstore/service/UserService.java
+++ src/main/java/com/modul2/bookstore/service/UserService.java
@@ -2,15 +2,26 @@ package com.modul2.bookstore.service;
 
 import com.modul2.bookstore.entities.User;
 import com.modul2.bookstore.repository.UserRepository;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User create(User user){
+    public User create(User user) {
+        String md5Hex = DigestUtils
+                .md5Hex(user.getPassword()).toUpperCase();
+
+        user.setPassword(md5Hex);
         return userRepository.save(user);
     }
 }
