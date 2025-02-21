cc5cf8e99cfd62178ac006ec5fb0c21f43be0884
alexandra <alexandra.dolca@student.upt.ro>
2025-02-21 17:58:59 +0300

    Library endpoints: find, delete, update

--- src/main/java/com/modul2/bookstore/controller/LibraryController.java
+++ src/main/java/com/modul2/bookstore/controller/LibraryController.java
@@ -1,13 +1,19 @@
 package com.modul2.bookstore.controller;
 
 import com.modul2.bookstore.dto.BookDTO;
+import com.modul2.bookstore.dto.LibrarianDTO;
+import com.modul2.bookstore.dto.LibraryDTO;
+import com.modul2.bookstore.entities.Librarian;
 import com.modul2.bookstore.entities.Library;
 import com.modul2.bookstore.mapper.BookMapper;
+import com.modul2.bookstore.mapper.LibrarianMapper;
 import com.modul2.bookstore.mapper.LibraryMapper;
 import com.modul2.bookstore.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/libraries")
@@ -25,6 +31,32 @@ public class LibraryController {
     public ResponseEntity<?> removeBookFromLibrary(@PathVariable(name = "libraryId") Long libraryID, @PathVariable(name = "bookId") Long bookID){
         libraryService.removeBookToLibrary(libraryID, bookID);
         return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/{libraryId}")
+    public ResponseEntity<?> getById(@PathVariable(name = "libraryId") Long libraryIdToSearchFor) {
+        Library foundLibrary = libraryService.getById(libraryIdToSearchFor);
+        return ResponseEntity.ok(LibraryMapper.library2LibraryDto(foundLibrary));
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> findAll() {
+        List<Library> library = libraryService.findAll();
+        return ResponseEntity.ok(library.stream().map(LibraryMapper::library2LibraryDto).toList());
+    }
+
+    @DeleteMapping("/{libraryId}")
+    public ResponseEntity<?> deleteById(@PathVariable(name = "libraryId") Long libraryIdToDelete) {
+        libraryService.deleteById(libraryIdToDelete);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{libraryId}")
+    public ResponseEntity<?> updateById(@PathVariable(name = "libraryId") Long libraryIdToUpdate, @RequestBody LibraryDTO libraryBody) {
+        Library libraryEntity = LibraryMapper.libraryDto2Library(libraryBody);
+        Library updatedLibrary = libraryService.updateById(libraryIdToUpdate, libraryEntity);
+        return ResponseEntity.ok(LibraryMapper.library2LibraryDto(updatedLibrary));
     }
 
 }
--- src/main/java/com/modul2/bookstore/service/LibraryService.java
+++ src/main/java/com/modul2/bookstore/service/LibraryService.java
@@ -1,11 +1,18 @@
 package com.modul2.bookstore.service;
 
 import com.modul2.bookstore.entities.Book;
+import com.modul2.bookstore.entities.Librarian;
 import com.modul2.bookstore.entities.Library;
 import com.modul2.bookstore.repository.LibraryRepository;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class LibraryService {
@@ -31,5 +38,30 @@ public class LibraryService {
 
         library.getBooks().remove(bookToRemove);
         libraryRepository.save(library);
+    }
+
+
+    public Library getById(Long libraryId){
+        return libraryRepository.findById(libraryId)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    public List<Library> findAll() {
+        return libraryRepository.findAll();
+    }
+
+    public void deleteById(Long libraryId) {
+        libraryRepository.deleteById(libraryId);
+    }
+
+    public Library updateById(Long libraryId, Library libraryBody) {
+        Library updatedLibrary = libraryRepository.findById(libraryId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        updatedLibrary.setName(libraryBody.getName());
+        updatedLibrary.setAddress(libraryBody.getAddress());
+        updatedLibrary.setPhoneNumber(libraryBody.getPhoneNumber());
+
+        return libraryRepository.save(updatedLibrary);
     }
 }
