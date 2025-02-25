ec6b640e84dd0e0a2acfa27521d30d35e8777752
alexandra <alexandra.dolca@student.upt.ro>
2025-02-25 19:55:22 +0300

    Exemplary - endpoints

---
+++ src/main/java/com/modul2/bookstore/mapper/ExemplaryMapper.java
@@ -0,0 +1,30 @@
+package com.modul2.bookstore.mapper;
+
+import com.modul2.bookstore.dto.CreateExemplaryDTO;
+import com.modul2.bookstore.dto.ExemplaryDTO;
+import com.modul2.bookstore.entities.Exemplary;
+
+public class ExemplaryMapper {
+    public static Exemplary exemplaryDto2Exemplary(ExemplaryDTO exemplaryDTO) {
+        Exemplary exemplary = new Exemplary();
+        exemplary.setPublisher(exemplaryDTO.getPublisher());
+        exemplary.setMaxBorrowDays(exemplaryDTO.getMaxBorrowDays());
+        return exemplary;
+    }
+
+    public static ExemplaryDTO exemplary2ExemplaryDto(Exemplary exemplary) {
+        ExemplaryDTO exemplaryDTO = new ExemplaryDTO();
+        exemplaryDTO.setId(exemplary.getId());
+        exemplaryDTO.setPublisher(exemplary.getPublisher());
+        exemplaryDTO.setMaxBorrowDays(exemplary.getMaxBorrowDays());
+        return exemplaryDTO;
+    }
+
+    public static Exemplary createExemplaryDtoToExemplary(CreateExemplaryDTO dto) {
+        Exemplary exemplary = new Exemplary();
+        exemplary.setPublisher(dto.getPublisher());
+        exemplary.setMaxBorrowDays(dto.getMaxBorrowDays());
+        return exemplary;
+    }
+}
+
